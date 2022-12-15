c50bfb972028a7e3608f2a39ad3431e7c20a8b9f
knightn <120507925+knightn@users.noreply.github.com>
2022-12-15 23:28:03 +0530

    Update p8sjfrrfcfs.cpp

--- p8sjfrrfcfs.cpp
+++ p8sjfrrfcfs.cpp
@@ -178,3 +178,125 @@ cin>>q;
 avg(p,n,b,q);
 return 0;
 }
+//nonpremetive
+using namespace std;
+int main()
+{
+cout<<"--Non-Preemptive Priority Based Scheduling Algorithm--\n";
+int b[20],p[20],w[20],t[20],pr[20],i,j,n,total=0,pos,aw,at;
+cout<<"\nEnter Number of Processes: ";
+cin>>n;
+cout<<"\nEnter Burst Time andPriority:- \n";
+for (int i=0;i<n;i++)
+{
+p[i]=i+1;
+}
+for (i=0;i<n;i++)
+{
+cout<<"Burst Time of P"<<i+1<<":- ";
+cin>>b[i];
+}
+for (i=0;i<n;i++)
+{
+cout<<"Priority of P"<<i+1<<":- ";
+cin>>pr[i];
+}
+for (i=0;i<n;i++)
+{
+pos=i;
+for (j=i+1;j<n;j++)
+{
+if (pr[j]<pr[pos])
+pos=j;
+}
+swap(pr[i],pr[pos]);
+swap(b[i],b[pos]);
+swap(p[i],p[pos]);
+}
+w[0]=0;
+for (i=1;i<n;i++)
+  w[i]=0;
+for (j=0;j<n;j++)
+{
+w[i] += b[j];
+total += w[i];
+}
+}
+aw=total/n;
+total=0;
+cout<<"\nProcess\t Burst Time \tWaiting Time\tTurnaround Time";
+for (i=0;i<n;i++)
+{
+t[i]=b[i]+w[i];
+total +=t[i];
+cout<<"\nP["<<p[i]<<"]\t\t "<<b[i]<<"\t\t
+"<<w[i]<<"\t\t\t"<<t[i];
+}
+at=total/n;
+cout<<"\n\nAverage Waiting Time= "<<aw;
+cout<<"\nAverage Turnaround Time="<<at;
+return 0;
+//preemtive
+#include <iostream>
+using namespace std;
+int main()
+{
+cout <<"--Preemptive Priority Based Scheduling Algorithm--\n";
+int a[10],b[10],x[10];
+int w[10],t[10],c[10],p[10];
+int i,j,min,count=0,time,n;
+float at=0,aw=0,end;
+cout<<"\nEnter the number of Processes:- ";
+cin>>n;
+for (i=0;i<n;i++)
+{
+cout<<"\nArrival time of P"<<i+1<<":- ";
+cin>>a[i];
+}
+for (i=0;i<n;i++)
+{
+cout<<"\nBurst time of P"<<i+1<<":- ";
+cin>>b[i];
+}
+for (i=0;i<n;i++)
+{
+cout<<"\nPriority time of P"<<i+1<<":- ";
+cin>>p[i];
+}
+for (i=0;i<n;i++)
+{
+x[i]=b[i];
+}
+p[9]=-1;
+for (time=0;count!=n;time++)
+{
+min=9;
+for(i=0;i<n;i++)
+{
+if (a[i]<=time&&p[i]>p[min]&&b[i]>0)
+Page | 15
+min=i;
+}
+b[min]--;
+if (b[min]==0)
+{ count++;
+end=time+1;
+c[min] = end;
+w[min] = end-a[min]-x[min];
+t[min] = end-a[min];
+}
+}
+cout<<"Process"<<"\t"<<"Burst-time"<<"\t"<<"Arrivaltime"<<"\
+t"<<"Waiting-time"<<"\t"<<"Turnaroundtime"<<"\
+t"<<"Completion-time"<<"\t"<<"Priority-time"<<endl;
+for (i=0;i<n;i++)
+{
+cout<<"P"<<i+1<<"\t\t"<<x[i]<<"\t\t"<<a[i]<<"\t\t"<<w[i]<<"\t\t"<
+<t[i]<
+<"\t\t"<<c[i]<<"\t\t"<<p[i]<<endl;
+aw=aw+w[i];
+at=at+t[i];
+}
+cout<<"\nAverage Waiting time= "<<aw/n;
+cout<<"\nAverage Turnaround time= "<<at/n<<endl;
+}
